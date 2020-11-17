cdf97b78c18c8c881ca6f3c952ed923451230ff9
esposni <esposni@usi.ch>
2020-11-17 19:43:04 +0100

    modified run_bench.sh

--- test1/src/run_membench.sh
+++ test1/src/run_membench.sh
@@ -3,13 +3,14 @@
 #SBATCH --job-name=membench
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
-#SBATCH --output=membench-%j.out
-#SBATCH --error=membench-%j.err
+#SBATCH --output=res/membench-timeline.out
+#SBATCH --error=res/membench-timeline.err
+#SBATCH --exclusive
 
 
 
 #likwid-perfctr -C S0:1 -g BRANCH -g L3 -O  -o likwid_res ./membench
-
-#for i in {1..2} do
-	likwid-perfctr -C S0:0 -g L3 -m ./membench > res/res.txt
-#done
+# Maker API
+#likwid-perfctr -C S0:0 -g L3  -m  ./membench
+#Time-line mode
+likwid-perfctr -C S0:0 -g L3 -t 0.5s ./membench 
