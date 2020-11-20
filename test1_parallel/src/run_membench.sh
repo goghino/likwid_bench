4a761ea0e5c4e8cc2ba4790b9fa63d09ea61f9ab
esposni <esposni@usi.ch>
2020-11-20 20:34:59 +0100

    parallel test

--- test1_parallel/src/run_membench.sh
+++ test1_parallel/src/run_membench.sh
@@ -3,14 +3,17 @@
 #SBATCH --job-name=membench
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
-#SBATCH --output=res/test1_parallel.out
-#SBATCH --error=res/test1_parallel.err
+#SBATCH --ntasks=10
+#SBATCH --output=res/test_parallel.out
+#SBATCH --error=res/test_parallel.err
 #SBATCH --exclusive
 
 
 
 #Time-line mode
-srun likwid-perfctr -C S0:0 -g L3 -t 0.5s  -o res/membench-timeline1.out -e res/membench-timeline-1.err  ./membench &
-srun likwid-perfctr -C S0:0 -g L3 -t 0.5s -o res/membench-timeline2.out -e res/membench-timeline-2.err ./membench &
+#for i in {1..10}
+#do
+srun likwid-perfctr -C S0:0 -g L3 -t 0.5s  ./membench &
+#done
 
 wait
