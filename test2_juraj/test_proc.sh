0b762849899de57c12583b944c865ab398b31bd5
Juraj Kardos <juraj.kardos@usi.ch>
2020-11-20 13:27:14 +0100

    Parametrize scripts

--- test2_juraj/test_proc.sh
+++ test2_juraj/test_proc.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#CLUSTER=ics
+#NODE=icsnode30
+#NCPUS=20
+
+CLUSTER=dxt
+NODE=STACK-CALC1
+NCPUS=64
+
 # run the stream using multiple processes
-for n in {0..19}
+for n in `seq 0 $((NCPUS-1))`
 do
 
 N=$((n + 1))
-mkdir -p results/proc$N
+mkdir -p results/$CLUSTER/proc$N
 
 for i in $(seq 0 $n)
 do
@@ -16,13 +24,13 @@ sbatch <<-_EOF
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --time=0:05:00
-#SBATCH --output=results/proc$N/proc_${i}.out
-#SBATCH --error=results/proc$N/proc_${i}.err
-#SBATCH --nodelist=icsnode30
-#SBATCH --mem=3GB
+#SBATCH --output=results/$CLUSTER/proc$N/proc_${i}.out
+#SBATCH --error=results/$CLUSTER/proc$N/proc_${i}.err
+#SBATCH --nodelist=$NODE
+#SBATCH --mem=2GB
 #//SBATCH --reservation=hpc_wednesday
 
-module load likwid
+#module load likwid
 export OMP_NUM_THREADS=1
 printf "Process $i\n"
 hostname
@@ -41,6 +49,6 @@ fi
 _EOF
 done
 
-read -p "Press any key when >>watch squeue | grep icsnode30<< is empty..."
+read -p "Press any key when >>watch squeue | grep $NODE<< is empty..."
 
 done
--- test2_juraj/test_thread.sh
+++ test2_juraj/test_thread.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+#CLUSTER=ics
+#NODE=icsnode30
+#NCPUS=20
+#NUMACORES=10
+
+CLUSTER=dxt
+NODE=STACK-CALC1
+NCPUS=64
+NUMACORES=32
+
 # run the single stream as multithread application
-for i in {1..20}
+for i in `seq 1 $NCPUS`
 do
 
-ii=$((i-10))
+ii=$((i-$NUMACORES))
+
+D=$((1000*NUMACORES))MB
 
 Da=$((1000*i))MB
 Db=$((1000*ii))MB
@@ -15,9 +27,9 @@ sbatch <<-_EOF
 #SBATCH --cpus-per-task=${i}
 #SBATCH --nodes=1
 #SBATCH --time=0:05:00
-#SBATCH --output=results/thread_${i}.out
-#SBATCH --error=results/thread_${i}.err
-#xxxSBATCH --nodelist=icsnode30
+#SBATCH --output=results/$CLUSTER/thread_${i}.out
+#SBATCH --error=results/$CLUSTER/thread_${i}.err
+#xxxSBATCH --nodelist=$NODE
 #SBATCH --exclusive
 
 module load likwid
@@ -29,12 +41,13 @@ printf "Data S1: $Db\n"
 
 # run the experiment, local data size 1000MB per thread, 100 iterations
 
-if [ $i -lt 11  ]; then
-    #first 10 threads are placed on S0, Da=1000*i
+if [ $i -le $NUMACORES  ]; then
+    #first NUMACORES threads are placed on S0, Da=1000*i
     likwid-perfctr -C E:N:$i -g L3 likwid-bench -i 100 -t triad -w S0:$Da:$i
 else
-    #additional threads are located at socket S1, Db=1000*ii, first 10 threads are run on S0
-    likwid-perfctr -C E:N:$i -g L3 likwid-bench -i 100 -t triad -w S0:10000MB:10 -w S1:$Db:$ii
+    #additional threads are located at socket S1, Db=1000*ii
+    #first NUMACORES threads are run on S0
+    likwid-perfctr -C E:N:$i -g L3 likwid-bench -i 100 -t triad -w S0:$D:$NUMACORES -w S1:$Db:$ii
 fi
 _EOF
 done
